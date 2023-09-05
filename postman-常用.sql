5438ac2e1f7a1f7580f8da83ce5d1da0b1a23bef
小虎 <xiaohu_gz@126.com>
2023-09-06 01:39:55 +0800

    settings

---
+++ postman-常用.sql
@@ -0,0 +1,12 @@
+flink async IO和自己在MapFunction中使用线程池的区别
+https://blog.csdn.net/u011624157/article/details/122947739
+
+OKHttp系列解析(一) Okio源码解析
+https://www.jianshu.com/p/5061860545ef
+
+flink async IO
+https://github.com/apache/flink/blob/master/flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/async/AsyncIOExample.java
+
+Flink源码分析(二)：Async I/O 实现原理
+https://bbs.huaweicloud.com/blogs/189300
+
